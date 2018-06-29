e02d1b36603ee24574ecbeb460f8624c663053d0
Jonathan Fine <finej@purdue.edu>
2018-06-29 14:38:34 -0400

    Additional protein

--- test/residue_count.cpp
+++ test/residue_count.cpp
@@ -9,10 +9,10 @@ using namespace benchmarker;
 TEST_CASE("Residue Count") {
     ResnCount counter;
     auto traj = chemfiles::Trajectory("files/5w1d.pdb", 'r');
-    auto test1 = traj.read();
+    auto test = traj.read();
 
 
-    retreive_residue_counts(test1, counter);
+    retreive_residue_counts(test, counter);
 
     CHECK(counter.size() == 22);
 
@@ -34,10 +34,21 @@ TEST_CASE("Residue Count") {
     CHECK(counter["SER"] == 27);
     CHECK(counter["THR"] == 37);
     CHECK(counter["TYR"] == 19);
-    //CHECK(counter["TRP"] == 0);
+    CHECK(counter["TRP"] == 0);
     CHECK(counter["MET"] == 4);
 
     CHECK(counter["HOH"] == 1);
     CHECK(counter["CA"]  == 6);
     CHECK(counter["K"]   == 1);
+
+    traj = chemfiles::Trajectory("files/1AAQ.mmtf", 'r');
+    test = traj.read();
+
+    retreive_residue_counts(test, counter);
+    CHECK(counter.size() == 24);
+
+    // Check the simple updates
+    CHECK(counter["HOH"] == 2);
+    CHECK(counter["PSI"] == 1);
+    CHECK(counter["TRP"] == 4);
 }
