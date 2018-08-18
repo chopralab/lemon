839b467e56e8267e21ab45e3b34557c4a97aca21
Jonathan Fine <finej@purdue.edu>
2018-08-18 15:17:12 -0400

    Added program to score all poses with Vina

---
+++ progs/vinascore_all.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <sstream>
+
+#include <boost/filesystem.hpp>
+
+#include <chemfiles.hpp>
+
+#include "lemon/archive_run.hpp"
+#include "lemon/count.hpp"
+#include "lemon/entries.hpp"
+#include "lemon/hadoop.hpp"
+#include "lemon/options.hpp"
+#include "lemon/prune.hpp"
+#include "lemon/score.hpp"
+#include "lemon/select.hpp"
+
+int main(int argc, char* argv[]) {
+    lemon::Options o(argc, argv);
+
+    auto worker = [](const chemfiles::Frame& complex,
+                     const std::string& pdbid) {
+
+        // Selection phase
+        auto smallm = lemon::select_small_molecules(complex);
+
+        // Pruning phase
+        lemon::remove_identical_residues(complex, smallm);
+        lemon::remove_cofactors(complex, smallm, lemon::common_cofactors);
+        lemon::remove_cofactors(complex, smallm, lemon::linear_molecules);
+
+        // Output phase
+        const auto residues = complex.topology().residues();
+        std::set<size_t> proteins;
+        for (size_t i = 0; i < complex.topology().residues().size(); ++i) {
+            proteins.insert(i);
+        }
+
+        for (auto smallm_id : smallm) {
+            auto prot_copy = proteins;
+
+            lemon::keep_interactions(complex, smallm, prot_copy, 8.0);
+            prot_copy.erase(smallm_id);
+
+            auto vscore =
+                lemon::xscore::vina_score(complex, smallm_id, prot_copy);
+
+            std::stringstream ss;
+            ss << pdbid << "\t" << residues[smallm_id].name() << "\t"
+               << vscore.g1 << "\t" << vscore.g2 << "\t" << vscore.hydrogen
+               << "\t" << vscore.hydrophobic << "\t" << vscore.rep << "\n";
+            std::cout << ss.str();
+        }
+    };
+
+    auto p = o.work_dir();
+    auto ncpu = o.npu();
+    auto entries = o.entries();
+
+    if (!boost::filesystem::is_directory(p)) {
+        std::cerr << "You must supply a valid directory" << std::endl;
+        return 2;
+    }
+
+    if (!entries.empty()) {
+        if (!boost::filesystem::is_regular_file(entries)) {
+            std::cerr << "You must supply a valid entries file" << std::endl;
+            return 1;
+        }
+
+        lemon::PDBIDVec vec;
+        lemon::read_entry_file(entries, vec);
+        boost::filesystem::current_path(p);
+        lemon::run_archive(worker, vec, ncpu, 1);
+    } else {
+        lemon::run_hadoop(worker, p, ncpu);
+    }
+}
