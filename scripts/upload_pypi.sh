08268c310ea79d41dc5a02e31609bf88d47c528a
Jonathan Fine <finej@purdue.edu>
2019-01-26 13:40:23 -0500

    Begin migration to Normal PyPI

--- scripts/upload_pypi.sh
+++ scripts/upload_pypi.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-echo -e "[distutils]" >> ~/.pypirc
-echo -e "index-servers =" >> ~/.pypirc
-echo -e "    test" >> ~/.pypirc
-echo -e "" >> ~/.pypirc
-echo -e "[test]" >> ~/.pypirc
-echo -e "repository = https://test.pypi.org/legacy/" >> ~/.pypirc
-echo -e "username = frodofine" >> ~/.pypirc
+echo -e "[pypi]" >> ~/.pypirc
+echo -e "username = chopralab" >> ~/.pypirc
 echo -e "password = $PYPI_PASSWORD" >> ~/.pypirc
 
 pip install twine
-twine upload --repository test dist/*
+twine upload dist/*
