47bf5f0fa48c9a1868ebb74ce5e6b0eff7d6e964
Jonathan Fine <finej@purdue.edu>
2019-03-17 14:26:09 -0400

    More workflow more flexible

--- progs/interactions/dmfase_metal.cpp
+++ progs/interactions/dmfase_metal.cpp
@@ -16,6 +16,8 @@ int main(int argc, char* argv[]) {
     auto ref = chemfiles::Trajectory(reference).read();
     auto ref_fe1 = lemon::select::specific_residues(ref, {"FE"});
     auto ref_res = lemon::select::specific_residues(ref, {"GLU", "HIS", "TYR"});
+
+    // Only the first iron from this file
     lemon::prune::keep_interactions(ref, ref_res, {*ref_fe1.begin()}, distance);
 
     std::vector<chemfiles::Vector3D> x;
@@ -31,8 +33,10 @@ int main(int argc, char* argv[]) {
                                     const std::string& pdbid) {
 
         // Selection phase
-        auto metals = lemon::select::specific_residues(entry, {"FE"});
-        auto resids = lemon::select::specific_residues(entry, {"GLU", "HIS", "TYR"});
+        auto metals = lemon::select::metal_ions(entry);
+        auto resids = lemon::select::specific_residues(entry,
+            {"GLU", "ASP", "HIS", "TYR", "SER", "CYS", "MET", "THR", "LYS"}
+        );
 
         std::string result;
 
@@ -44,33 +48,32 @@ int main(int argc, char* argv[]) {
         for (auto metal : metals) {
             auto resids2 = resids;
             lemon::prune::keep_interactions(entry, resids2, {metal}, distance);
-            if (resids2.size() != x.size()) {
-                continue;
-            }
             std::vector<chemfiles::Vector3D> y;
+            result += entry.topology().residues()[metal].name() + " ";
             for (auto res : resids2) {
                 auto cur_res = entry.topology().residues()[res];
                 auto loc = lemon::tmalign::find_element_by_name(entry, cur_res, "CA");
                 y.push_back(entry.positions()[loc]);
-                result += cur_res.name() + "_" + std::to_string(*cur_res.id()) + " ";
+                result += cur_res.name() + std::to_string(*cur_res.id()) + ":" + cur_res.get("chainname")->as_string() + " ";
             }
 
             chemfiles::Matrix3D u = chemfiles::Matrix3D::unit();
             chemfiles::Vector3D t;
             int err;
 
-            auto rms = lemon::tmalign::kabsch(w, x, y, y.size(), u, t, err);
+            while (y.size() >= x.size()) {
+                auto rms = lemon::tmalign::kabsch(w, x, y, x.size(), u, t, err);
+                result += std::to_string(rms) + " ";
+                y.erase(y.begin());
+            }
 
-            result += std::to_string(rms) + " " + pdbid + "\n";
+            result += pdbid + "\n";
         }
-std::cout << result;
+
         // Output phase
         return result;
     };
 
-    auto frame = chemfiles::Trajectory(o.entries()).read();
-    worker(std::move(frame), "test");
-
-    //auto collector = lemon::print_combine(std::cout);
-    //return lemon::launch(o, worker, collector);
+    auto collector = lemon::print_combine(std::cout);
+    return lemon::launch(o, worker, collector);
 }
